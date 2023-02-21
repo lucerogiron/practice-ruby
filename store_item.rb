c0b48a1042e4331b0ca203d614777054a1078caa
Lucero <gironlucero98@gmail.com>
2023-02-21 12:38:35 -0800

    store items

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,39 @@
+#ruby
 
-tshirt = {color =>  "red", price =>  11.99, size => "M"}
-jeans = {color "blue": , price 24.99: , size : "S"}
-denim_jacket = {color : "cream", price : 30.00, size : "M"}
+tshirt = {
+  :color => "red",
+  :price => 11.99,
+  :size => "Medium",
+}
+
+jeans = { :color => "blue",
+          :price => 24.99,
+          :size => "Small" }
+
+denim_jacket = {
+  :color => "cream",
+  :price => 30.00,
+  :size => "Medium",
+}
+
+#java
+
+tshirt = {
+  color: "red",
+  price: 11.99,
+  size: "Medium",
+}
+
+jeans = {
+  color: "blue",
+  price: 24.99,
+  size: "Small",
+}
+
+denim_jacket = {
+  color: "cream",
+  price: 30.00,
+  size: "Medium",
+}
+
+puts tshirt[:color]
