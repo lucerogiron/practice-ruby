c03b8be9f213d096e1cc80a9c7aa60ec7bf6606d
Lucero <gironlucero98@gmail.com>
2023-02-22 09:55:46 -0800

    added class

--- store_item.rb
+++ store_item.rb
@@ -1,39 +1,72 @@
-#ruby
+# #ruby
 
-tshirt = {
-  :color => "red",
-  :price => 11.99,
-  :size => "Medium",
-}
+# tshirt = {
+#   :color => "red",
+#   :price => 11.99,
+#   :size => "Medium",
+# }
 
-jeans = { :color => "blue",
-          :price => 24.99,
-          :size => "Small" }
+# jeans = { :color => "blue",
+#           :price => 24.99,
+#           :size => "Small" }
 
-denim_jacket = {
-  :color => "cream",
-  :price => 30.00,
-  :size => "Medium",
-}
+# denim_jacket = {
+#   :color => "cream",
+#   :price => 30.00,
+#   :size => "Medium",
+# }
 
-#java
+# #java
 
-tshirt = {
-  color: "red",
-  price: 11.99,
-  size: "Medium",
-}
+# tshirt = {
+#   color: "red",
+#   price: 11.99,
+#   size: "Medium",
+# }
 
-jeans = {
-  color: "blue",
-  price: 24.99,
-  size: "Small",
-}
+# jeans = {
+#   color: "blue",
+#   price: 24.99,
+#   size: "Small",
+# }
 
-denim_jacket = {
-  color: "cream",
-  price: 30.00,
-  size: "Medium",
-}
+# denim_jacket = {
+#   color: "cream",
+#   price: 30.00,
+#   size: "Medium",
+# }
 
-puts tshirt[:color]
+class Storeitem
+  attr_reader :name, :color, :price, :size
+  attr_writer :price
+
+  def initialize(name, color, price, size)
+    @name = name
+    @color = color
+    @price = price
+    @size = size
+  end
+
+  # def color
+  #   @color
+  # end
+
+  # def price
+  #   @price
+  # end
+
+  # def size
+  #   @size
+  # end
+
+  # def price=(price)
+  #   @price = price
+  # end
+end
+
+item = Storeitem.new("denim_pants", "blue", 49.99, "medium")
+
+puts item.color
+puts item.size
+item.price = 24.99
+puts item.price
