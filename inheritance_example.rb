ce1ca7104b419c5a410e00a22afc1124f200e822
Lucero <gironlucero98@gmail.com>
2023-02-22 11:06:22 -0800

    shortened duplicates in code

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = "north"
@@ -15,31 +15,61 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
+
+# def honk_horn= (honk_horn)
+#   @
+# end
+
+class Car < Vehicle
+  # def initialize
+  #   @speed = 0
+  #   @direction = "north"
+  # end
+
+  # def brake
+  #   @speed = 0
+  # end
+
+  # def accelerate
+  #   @speed += 10
+  # end
+
+  # def turn(new_direction)
+  #   @direction = new_direction
+  # end
 
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = "north"
-  end
+class Bike < Vehicle
+  # def initialize
+  #   @speed = 0
+  #   @direction = "north"
+  # end
 
-  def brake
-    @speed = 0
-  end
+  # def brake
+  #   @speed = 0
+  # end
 
-  def accelerate
-    @speed += 10
-  end
+  # def accelerate
+  #   @speed += 10
+  # end
 
-  def turn(new_direction)
-    @direction = new_direction
-  end
+  # def turn(new_direction)
+  #   @direction = new_direction
+  # end
 
-  def ring_bell
-    puts "Ring ring!"
+  def honk_horn
+    puts "beep beep!"
   end
 end
+
+bike = Bike.new
+
+p bike.accelerate
+p bike.accelerate
+p bike.accelerate
+bike.honk_horn
