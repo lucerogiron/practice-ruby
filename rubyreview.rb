3b921afc0545b57e9247de9b8989ffd4a03caaed
Lucero <gironlucero98@gmail.com>
2023-02-24 13:42:36 -0800

    uncommented out code block

--- rubyreview.rb
+++ rubyreview.rb
@@ -1,20 +1,55 @@
-newarr = []
-5.times do
-  puts "Enter a favorite food"
-  food = gets.chomp
-  newarr << food
-end
+#Excercises: Ruby review
+#1. Variables, Array, and Loops
 
-# index = 0
-# while index < newarr.length
-#   puts "I love #{newarr[index]}"
-#   index = index + 1
+# newarr = []
+# 5.times do
+#   puts "Enter a favorite food"
+#   food = gets.chomp
+#   newarr << food
 # end
 
-index = 0
-num = 1
-while index < newarr.length
-  puts "#{num}. #{newarr[index]}"
-  index = index + 1
-  num = num + 1
+# # index = 0
+# # while index < newarr.length
+# #   puts "I love #{newarr[index]}"
+# #   index = index + 1
+# # end
+
+# index = 0
+# num = 1
+# while index < newarr.length
+#   puts "#{num}. #{newarr[index]}"
+#   index = index + 1
+#   num = num + 1
+# end
+
+#3. Hashes
+
+newarr = []
+
+2.times do
+  person = {}
+
+  puts "Please enter a first name:"
+  first_name = gets.chomp
+  person[:fname] = first_name
+
+  puts "Please enter a last name:"
+  last_name = gets.chomp
+  person[:lname] = last_name
+
+  puts "Please enter an email:"
+  email = gets.chomp
+  person[:email] = email
+
+  num = Random.rand(10000000000)
+  person[:account] = num
+
+  newarr << person
+end
+
+newarr.each do |person|
+  puts "FIRST NAME: #{person[:fname]}"
+  puts "LAST NAME: #{person[:lname]}"
+  puts "EMAIL: #{person[:email]}"
+  puts "ACCT #: #{person[:account]}"
 end
