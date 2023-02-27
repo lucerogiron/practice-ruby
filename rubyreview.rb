680007c2e11de0e3918604e6d8428671aca71dc6
lucerogiron <gironlucero98@gmail.com>
2023-02-27 08:57:09 -0800

    added work journal promts

--- rubyreview.rb
+++ rubyreview.rb
@@ -1,4 +1,6 @@
 #Excercises: Ruby review
+# https://gist.github.com/peterxjang/ba4fe8368a62e3f93768df01f6f4a963 (file ref)
+
 #1. Variables, Array, and Loops
 
 # newarr = []
@@ -24,32 +26,49 @@
 
 #3. Hashes
 
-newarr = []
+# newarr = []
 
-2.times do
-  person = {}
+# 2.times do
+#   person = {}
 
-  puts "Please enter a first name:"
-  first_name = gets.chomp
-  person[:fname] = first_name
+#   puts "Please enter a first name:"
+#   first_name = gets.chomp
+#   person[:fname] = first_name
 
-  puts "Please enter a last name:"
-  last_name = gets.chomp
-  person[:lname] = last_name
+#   puts "Please enter a last name:"
+#   last_name = gets.chomp
+#   person[:lname] = last_name
 
-  puts "Please enter an email:"
-  email = gets.chomp
-  person[:email] = email
+#   puts "Please enter an email:"
+#   email = gets.chomp
+#   person[:email] = email
 
-  num = Random.rand(10000000000)
-  person[:account] = num
+#   num = Random.rand(10000000000)
+#   person[:account] = num
 
-  newarr << person
+#   newarr << person
+# end
+
+# newarr.each do |person|
+#   puts "FIRST NAME: #{person[:fname]}"
+#   puts "LAST NAME: #{person[:lname]}"
+#   puts "EMAIL: #{person[:email]}"
+#   puts "ACCT #: #{person[:account]}"
+# end
+
+#4. final review
+students_arr = []
+
+while true
+  puts "Please enter the students names, when finished please type 'done' :"
+
+  student = gets.chomp
+
+  if student == "done"
+    break
+  else
+    students_arr << student
+  end
 end
 
-newarr.each do |person|
-  puts "FIRST NAME: #{person[:fname]}"
-  puts "LAST NAME: #{person[:lname]}"
-  puts "EMAIL: #{person[:email]}"
-  puts "ACCT #: #{person[:account]}"
-end
+p
